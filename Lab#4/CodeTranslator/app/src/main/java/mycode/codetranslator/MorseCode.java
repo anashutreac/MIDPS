5f9f454db0e10bed66425ccb693e34a7ec8e9848
Ana Șutreac <ana_shutreac@yahoo.com>
2017-04-24 12:46:30 +0300

    Morse Code Design

---
+++ Lab#4/CodeTranslator/app/src/main/java/mycode/codetranslator/MorseCode.java
@@ -0,0 +1,13 @@
+package mycode.codetranslator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MorseCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_morse_code);
+    }
+}
